8f16923bba5a053c9ee67b109720ad3ff3998aaf
Hernan Clich <hernan.g.clich@gmail.com>
2025-08-26 20:12:00 -0300

    Exporting more functionality

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,13 +1,20 @@
 // Export providers
 export * from "./providers";
 
+// Export hooks
+export * from "./hooks";
+
 // Export constants and types that React users might need
 export { DYNAMIC_ENVIRONMENT_IDS, type DynamicEnvironment } from "../constants";
 
 // Re-export Dynamic components and hooks that React users might need
 export {
   DynamicUserProfile,
-  useDynamicContext,
+  DynamicWidget,
   useIsLoggedIn,
   useUserWallets,
+  useWalletOptions,
 } from "@dynamic-labs/sdk-react-core";
+
+// Re-export Dynamic ethers integration
+export { getSigner, getWeb3Provider } from "@dynamic-labs/ethers-v6";
