bd459ddea91c27055816e39a3168219dffcb906f
Hernan Clich <hernan.g.clich@gmail.com>
2025-08-29 15:06:19 -0300

    chore: Make ethers imports optional

---
+++ src/ethers.ts
@@ -0,0 +1,2 @@
+// Ethers.js integration for @zetachain/wallet
+export { getSigner, getWeb3Provider } from "@dynamic-labs/ethers-v6";
--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,6 @@ export * from "@dynamic-labs/global-wallet-client/features";
 // Export constants and types
 export { DYNAMIC_ENVIRONMENT_IDS, type DynamicEnvironment } from "./constants";
 
-// Framework-agnostic blockchain integrations
-export { getSigner, getWeb3Provider } from "@dynamic-labs/ethers-v6";
-
 // Export key ethereum utilities (excluding isEthereumWallet due to conflict with global-wallet-client)
 export {
   createConnector,
