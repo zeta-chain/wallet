552f33f31023be162b729e5b34513a564bc672d0
Hernan Clich <hernan.g.clich@gmail.com>
2025-09-04 15:18:29 -0300

    chore: Refactor env-based URL resolution

--- src/lib/registerSolanaStandard.ts
+++ src/lib/registerSolanaStandard.ts
@@ -3,17 +3,23 @@ import {
   registerWallet,
 } from "@dynamic-labs/global-wallet-client/solana";
 
+import { DYNAMIC_ENVIRONMENT_IDS } from "../constants";
 import { config } from "./config";
-import Wallet from "./wallet";
+import { getWallet } from "./wallet";
 
-export const registerSolanaStandard = () => {
+export const registerSolanaStandard = (
+  environment: keyof typeof DYNAMIC_ENVIRONMENT_IDS = "sandbox"
+) => {
+  const environmentId = DYNAMIC_ENVIRONMENT_IDS[environment];
+  const wallet = getWallet(environmentId);
+
   registerWallet(
     createSolanaWallet(
       {
         icon: config.walletIcon as `data:image/svg+xml;base64,${string}`,
         name: config.walletName,
       },
-      Wallet
+      wallet
     )
   );
 };
